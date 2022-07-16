20eb43b528207efbc3506468e8c5d45cabbdba72
Sunil Jain <sunil.1908@yahoo.co.in>
2022-07-16 09:53:51 +0000

    commit from node2

---
+++ src/main/java/com/vodafone/simplilearn/SimplilearnApplication.java
@@ -0,0 +1,13 @@
+package com.vodafone.simplilearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplilearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplilearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vodafone/simplilearn/SimplilearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vodafone.simplilearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplilearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
